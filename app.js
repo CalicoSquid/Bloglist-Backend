b62b21bdebc831952bb176f1ac37633e8b311cb4
Caleb Mow <immensedrama@gmail.com>
2024-01-02 20:23:19 +0100

    Added testing using Jest

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const router = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
+const router = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -17,11 +17,11 @@ mongoose
   .then(() => logger.info("Conected to MongoDB"))
   .catch((error) => logger.info("Error connecting to MongoDB", error.message));
 
-  app.use(cors());
-  app.use(express.static('dist'));
-  app.use(express.json());
-  app.use(middleware.requestLogger);
-app.use('/api/blogs', router);
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use("/api/blogs", router);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
